c35dcd55801e942da7cef42d4135290a0287d86d
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-19 13:49:02 -0400

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx"
 
 function App() {
   return (
-    <>
+    <div class="flex flex-col items-center">
       <Header />
-    </>
+      <Hero />
+    </div>
   );
 }
 export default App;
