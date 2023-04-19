d9fa9e43eeafef66ee2be1053fc445d16d1e1721
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-18 20:19:31 -0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div class="flex justify-end items-center w-4/5">
+      <ul class="flex gap-7">
+        <li><a href="#">/home</a></li>
+        <li><a href="#">/about</a></li>
+        <li><a href="#">/resume</a></li>
+      </ul>
+    </div>
+  );
+}
