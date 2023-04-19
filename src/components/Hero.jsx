89a140ed76c8c2edc3a62e6fed913b474d3fdb82
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-19 13:49:28 -0400

    add: hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+const Hero = () => {
+  return (
+    <div class="w-3/5 mt-40">
+      <p class="font-light text-emerald-200">Hi, my name is</p>
+      <h2 class="text-6xl font-semibold text-emerald-400">Nicholas Ronzel</h2>
+      <p class="text-5xl font-semibold text-emerald-200 mb-1 italic">
+        I love solving problems.
+      </p>
+      <p class="text-emerald-700">
+        Web Developer / Software Engineer / IT Professional
+      </p>
+      <p class="mt-4 text-emerald-200 w-1/2">
+        Transitioning from a decade-long IT career, I'm an aspiring software
+        engineer skilled in Javascript, HTML, CSS, Python, React & more.
+        Combining my technical expertise with a passion for innovation and
+        continuous learning, I'm excited to create impactful software solutions
+        in my new role.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
