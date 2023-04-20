aed1824b85644e6f629b4c30831bb76bbaa69bc6
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-19 22:53:58 -0400

    changed font size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div class="flex justify-end items-center w-4/5 font-medium mt-3">
-      <ul class="flex gap-7">
+      <ul class="flex gap-7 text-lg">
         <li><a href="#"><span class="font-light">/\/ </span>about</a></li>
         <li><a href="#"><span class="font-light">/\/ </span>projects</a></li>
         <li><a href="#"><span class="font-light">/\/ </span>resume</a></li>
