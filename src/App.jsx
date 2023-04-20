679af0f60b9e6169effdc47c03f451a12b52d048
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-19 22:54:40 -0400

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx"
+import Spacer from "./components/Spacer.jsx"
 
 function App() {
   return (
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center h-screen main">
       <Header />
       <Hero />
+      <Spacer />
     </div>
   );
 }
