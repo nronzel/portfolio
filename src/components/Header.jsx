feb0fa03d0adb616e83056ee4a8193918f855742
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-22 15:35:08 -0400

    add: smooth scrolling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 export default function Header() {
+  const scrollToSection = (e, id) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div class="flex justify-end items-center w-4/5 font-medium mt-3">
       <ul class="flex gap-7 text-lg">
         <li>
           <span class="font-light">/\/ </span>
-          <a href="#about">about</a>
+          <a href="#about" onClick={() => scrollToSection("about")}>
+            about
+          </a>
         </li>
         <li>
           <span class="font-light">/\/ </span>
@@ -12,7 +21,9 @@ export default function Header() {
         </li>
         <li>
           <span class="font-light">/\/ </span>
-          <a href="#" class="underline underline-offset-4">resume</a>
+          <a href="#" class="underline underline-offset-4">
+            resume
+          </a>
         </li>
       </ul>
     </div>
