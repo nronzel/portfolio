3918afdd699e81c4907de029cb20b42702c82900
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-22 13:49:17 -0400

    refactor: proper scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx"
-import Spacer from "./components/Spacer.jsx"
+import Hero from "./components/Hero.jsx";
+import Spacer from "./components/Spacer.jsx";
+import AboutMe from "./components/AboutMe.jsx";
 
 function App() {
   return (
-    <div class="flex flex-col items-center h-screen main">
-      <Header />
-      <Hero />
-      <Spacer />
-    </div>
+    <>
+      <div class="w-full flex flex-col items-center h-full main-container">
+        <Header />
+        <Hero />
+        <Spacer />
+      </div>
+      <AboutMe />
+    </>
   );
 }
 export default App;
