ba4d40c07ca9a393568c111e9f1a4ae1d65a149e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-22 15:34:40 -0400

    add: more text and styles

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,33 @@
 const AboutMe = () => {
   return (
-    <div id="about" class="text-right w-9/12 pr-9 pt-60 border-r-8 border-emerald-500">
-      <p class="text-xs text-emerald-800">a little more</p>
-      <p class="text-4xl font-semibold">About Me</p>
+    <div
+      id="about"
+      class="
+      w-9/12
+      pr-9
+      pt-60
+      border-r-8
+      border-emerald-500
+      flex
+      flex-col
+      items-end
+      "
+    >
+      <p class="text-xs text-emerald-800 pr-32">a little more</p>
+      <p class="text-4xl font-semibold pr-9 pb-5">About Me</p>
+      <p class="text-emerald-100 max-w-3xl text-justify">
+        Ever since I was a kid, I've been captivated by the world of technology.
+        I can remember flipping through my mom's computer books, intrigued by
+        the content and diagrams even before I knew what any of it meant. This
+        curiosity lead me to the world of programming and computer science, and I've
+        been hooked ever since. After a fulfilling 10-year career in IT, I've
+        decided to pivot towards web development and software engineering. I'm
+        excited about this transition, as it allows me to blend my expertise
+        with my passion for creating innovative and engaging digital solutions.
+        I look forward to making a real impact in the world of technology and
+        enhancing the lives of users around the globe.
+      </p>
     </div>
   );
 };
-
-export default AboutMe
+export default AboutMe;
