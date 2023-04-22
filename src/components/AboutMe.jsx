1b941bdf2e7df77210973b2f342ff2ff2a51ef9e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-22 13:51:24 -0400

    add: about me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+  return (
+    <div id="about" class="text-right w-9/12 pr-9 pt-60 border-r-8 border-emerald-500">
+      <p class="text-xs text-emerald-800">a little more</p>
+      <p class="text-4xl font-semibold">About Me</p>
+    </div>
+  );
+};
+
+export default AboutMe
