8539d051145f3a8ab6d3f045891a93dcd151ed8e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-23 17:13:19 -0400

    refactor: icons can be passed as props
    you can now pass an array of icon names for whatever icons are needed on
    each card.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,22 +13,28 @@ import {
 } from "solid-icons/si";
 
 const ProjectCard = (props) => {
+  const icons = {
+    react: <FaBrandsReact color="#43c3ec" />,
+    vite: <SiVite />,
+    sass: <FaBrandsSass />,
+    css: <FaBrandsCss3 />,
+    html: <FaBrandsHtml5 />,
+    javascript: <SiJavascript />,
+    netlify: <SiNetlify />,
+    firebase: <SiFirebase />,
+    solid: <SiSolid />,
+  };
+
+  const renderIcons = () => {
+    return props.icons.map((icon) => icons[icon] || null);
+  };
+
   return (
-    <div class="text-5xl w-full">
-      <p>{props.title}</p>
+    <div class="w-full border rounded p-8 border-emerald-900">
       <img src={props.src} alt={props.alt} />
-      <p>{props.description}</p>
-      <div class="flex">
-        <FaBrandsReact />
-        <SiVite />
-        <FaBrandsSass />
-        <FaBrandsCss3 />
-        <FaBrandsHtml5 />
-        <SiJavascript />
-        <SiNetlify />
-        <SiFirebase />
-        <SiSolid />
-      </div>
+      <p class="text-3xl mt-2">{props.title}</p>
+      <p class="text-l">{props.description}</p>
+      <div class="flex justify-between text-4xl mt-2">{renderIcons}</div>
     </div>
   );
 };
