f0138fa65aebda23d1171214794f323ccf93656e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-23 16:36:28 -0400

    refactor: useIntersectionObserver
    cleaned up intersection observer logic with this package

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,55 +1,30 @@
-import { createEffect, createSignal, onCleanup } from "solid-js";
+import { createSignal } from "solid-js";
+import { useIntersectionObserver } from "solidjs-use";
 import "./styles/animations.css";
 
 const AboutMe = () => {
-  let aboutMeRef;
+  const [target, setTarget] = createSignal(null);
   const [isVisible, setIsVisible] = createSignal(false);
 
-  // visibility for the AboutMe section on scroll
-  createEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true);
-          } else {
-            setIsVisible(false);
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 1.0,
-      }
-    );
-
-    if (aboutMeRef) {
-      observer.observe(aboutMeRef);
-    }
-
-    onCleanup(() => {
-      if (aboutMeRef) {
-        observer.unobserve(aboutMeRef);
-      }
-    });
+  useIntersectionObserver(target, ([{ isIntersecting }]) => {
+    setIsVisible(isIntersecting);
   });
 
   return (
     <div
       id="about"
-      ref={aboutMeRef}
+      ref={setTarget}
       class="
-      w-9/12
       pr-9
+      pt-36
       pl-2
-      pt-60
       border-r-8
       border-b-2
       border-emerald-500
       flex
       flex-col
       items-end
+      w-full
       "
       className={`about-me ${
         isVisible() ? "fade-in-from-top" : "fade-out-to-top"
@@ -67,17 +42,21 @@ const AboutMe = () => {
       >
         About Me
       </p>
-      <p class="text-emerald-100 max-w-3xl text-justify mb-9">
+      <p class="text-emerald-100 max-w-2xl text-justify mb-9">
         Ever since I was a kid, I've been captivated by the world of technology.
         I can remember flipping through my mom's computer books, intrigued by
         the content and diagrams even before I knew what any of it meant. This
         curiosity eventually lead me to the world of programming and computer
-        science, and I've been hooked ever since. After a fulfilling 10+ year
-        career in IT, I've decided to pivot towards web development and software
-        engineering. I'm excited about this transition, as it allows me to blend
-        my expertise with my passion for creating innovative and engaging
-        digital solutions. I look forward to making a real impact in the world
-        of technology and enhancing the lives of users around the globe.
+        science, and I've been hooked ever since.{" "}
+        <span class="block mt-3">
+          {" "}
+          After a fulfilling 10+ year career in IT, I've decided to pivot
+          towards web development and software engineering. I'm excited about
+          this transition, as it allows me to blend my expertise with my passion
+          for creating innovative and engaging digital solutions. I look forward
+          to making a real impact in the world of technology and enhancing the
+          lives of users around the globe.
+        </span>
       </p>
     </div>
   );
