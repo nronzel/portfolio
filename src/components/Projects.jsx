3c74b04813f6b00139ffcc68f171755fc844fe3f
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-24 14:10:16 -0400

    add: signal to hold hovered icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,10 @@
+import { createSignal } from "solid-js";
+import IconArray from "./IconArray";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
+  const [hoveredIcons, setHoveredIcons] = createSignal([]);
+
   const testProj = {
     title: "title",
     src: "#",
@@ -12,52 +16,74 @@ const Projects = () => {
     <div
       class="
       flex
+      flex-col
+      justify-center
+      items-center
+      border-t-2
+      border-emerald-500
+      gap-3
+      "
+    >
+      <h2 id="projects" class="text-5xl mt-9">
+        Projects
+      </h2>
+      <IconArray hoveredIcons={hoveredIcons} />
+      <div
+        class="
+      flex
       justify-center
       items-center
       gap-8 w-full
       flex-wrap
-      border-t-2
-      border-emerald-500
       pt-9
+      relative
       "
-    >
-
-      <ProjectCard
-        title={testProj.title}
-        src={testProj.src}
-        alt={testProj.alt}
-        description={testProj.description}
-        icons={["solid", "vite", "css", "javascript"]}
-      />
-      <ProjectCard
-        title={testProj.title}
-        src={testProj.src}
-        alt={testProj.alt}
-        description={testProj.description}
-        icons={[
-          "teact",
-          "vite",
-          "sass",
-          "javascript",
-          "reactrouter",
-          "firebase",
-          "netlify",
-        ]}
-      />
-      <ProjectCard
-        title={testProj.title}
-        src={testProj.src}
-        alt={testProj.alt}
-        description={testProj.description}
-        icons={["html", "webpack", "css", "javascript", "react"]}
-      />
-      <ProjectCard
-        title={testProj.title}
-        src={testProj.src}
-        alt={testProj.alt}
-        description={testProj.description}
-        icons={["solid", "vite", "css", "javascript"]}
-      />
+      >
+        <ProjectCard
+          title={testProj.title}
+          src={testProj.src}
+          alt={testProj.alt}
+          description={testProj.description}
+          icons={["solid", "vite", "css", "javascript"]}
+          hoveredIcons={hoveredIcons}
+          setHoveredIcons={setHoveredIcons}
+        />
+        <ProjectCard
+          title={testProj.title}
+          src={testProj.src}
+          alt={testProj.alt}
+          description={testProj.description}
+          icons={[
+            "teact",
+            "vite",
+            "sass",
+            "javascript",
+            "reactrouter",
+            "firebase",
+            "netlify",
+          ]}
+          hoveredIcons={hoveredIcons}
+          setHoveredIcons={setHoveredIcons}
+        />
+        <ProjectCard
+          title={testProj.title}
+          src={testProj.src}
+          alt={testProj.alt}
+          description={testProj.description}
+          icons={["html", "webpack", "css", "javascript", "react"]}
+          hoveredIcons={hoveredIcons}
+          setHoveredIcons={setHoveredIcons}
+        />
+        <ProjectCard
+          title={testProj.title}
+          src={testProj.src}
+          alt={testProj.alt}
+          description={testProj.description}
+          icons={["solid", "vite", "css", "javascript"]}
+          hoveredIcons={hoveredIcons}
+          setHoveredIcons={setHoveredIcons}
+        />
+      </div>
     </div>
   );
 };
