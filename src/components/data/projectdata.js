ce9ce7d09ed6474cd30122e6196db474c9d14359
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-24 21:27:10 -0400

    add: project data file  contains all data needed for the project cards

---
+++ src/components/data/projectdata.js
@@ -0,0 +1,100 @@
+const projects = [
+  {
+    title: "MarkdDown",
+    src: "#",
+    alt: "markd-markdown-editor",
+    description: `Markdown editor with Firebase backend. Full CRUD
+      functionality & saved documents are encrypted for privacy!`,
+    sourcecode: "https://github.com/nronzel/markdown-editor",
+    link: "https://markddown.netlify.app/",
+    icons: [
+      "javascript",
+      "react",
+      "css",
+      "reactrouter",
+      "firebase",
+      "vite",
+      "jest",
+    ],
+  },
+  {
+    title: "My Portfolio",
+    src: "#",
+    alt: "my-portfolio",
+    description: `This portfolio was designed and built with love by me!
+      I used it as an opportunity to learn SolidJS as well.`,
+    sourcecode: "https://github.com/nronzel/portfolio",
+    link: "#",
+    icons: ["javascript", "solid", "tailwind", "css", "netlify", "vite"],
+  },
+  {
+    title: "Wthrr",
+    src: "#",
+    alt: "wthrr-weather-app",
+    description: `Minimalist weather app using Weather API. Written in
+      vanilla Javascript.`,
+    sourcecode: "https://github.com/nronzel/Wthr",
+    link: "https://nronzel.github.io/Wthr/",
+    icons: ["html", "css", "javascript", "webpack"],
+  },
+  {
+    title: "Knights Travails",
+    src: "#",
+    alt: "knights-travails",
+    description: `Given a chessboard and the knight piece, this app determines
+      the shortest path between any two points.`,
+    sourcecode: "https://github.com/nronzel/Knights-Travails",
+    link: "https://nronzel.github.io/Knights-Travails/",
+    icons: ["html", "sass", "javascript", "webpack"],
+  },
+  {
+    title: "Keeb Store",
+    src: "#",
+    alt: "keeb-store",
+    description: `Mock keyboard store with shopping cart.`,
+    sourcecode: "https://github.com/nronzel/shopping-cart",
+    link: "keebstore.netlify.app",
+    icons: [
+      "javascript",
+      "vite",
+      "html",
+      "chakraui",
+      "react",
+      "reactrouter",
+      "jest",
+      "netlify",
+    ],
+  },
+  {
+    title: "ResumeBuildr",
+    src: "#",
+    alt: "resume-builder",
+    description: `Fill out the sections of the form and watch your resume
+      be built in the preview.`,
+    sourcecode: "https://github.com/nronzel/resume-maker",
+    link: "https://nronzel.github.io/resume-maker/",
+    icons: ["javascript", "vite", "html", "chakraui", "react"],
+  },
+  {
+    title: "SpongeMem",
+    src: "#",
+    alt: "memory-game",
+    description: `Memory game with characters from Spongebob. What is your
+      highest score?`,
+    sourcecode: "https://github.com/nronzel/Memory-game",
+    link: "spongemem.netlify.app/",
+    icons: ["javascript", "vite", "html", "chakraui", "react", "netlify"],
+  },
+  {
+    title: "What Am I Working On?",
+    src: "#",
+    alt: "current-project",
+    description: `Currently building a restaurant waitlist/reservation app. This will
+      allow me to properly learn backend with Express and MongoDB and build a full stack MERN app.`,
+    sourcecode: "",
+    link: "",
+    icons: [],
+  },
+];
+
+export default projects;
