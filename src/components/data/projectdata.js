589083750bffbeb84d662c943ed9a41f372816f6
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-25 14:09:50 -0400

    add: thumbnail sources & image flag

--- src/components/data/projectdata.js
+++ src/components/data/projectdata.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     title: "MarkdDown",
-    src: "#",
+    src: "/markd.png",
     alt: "markd-markdown-editor",
     description: `Markdown editor with Firebase backend. Full CRUD
       functionality & saved documents are encrypted for privacy!`,
@@ -16,74 +16,80 @@ const projects = [
       "vite",
       "jest",
     ],
+    includeImage: true,
   },
   {
     title: "My Portfolio",
-    src: "#",
+    src: "/portfolio.png",
     alt: "my-portfolio",
     description: `This portfolio was designed and built with love by me!
       I used it as an opportunity to learn SolidJS as well.`,
     sourcecode: "https://github.com/nronzel/portfolio",
     link: "#",
     icons: ["javascript", "solid", "tailwind", "css", "netlify", "vite"],
+    includeImage: true,
   },
   {
     title: "Wthrr",
-    src: "#",
+    src: "/wthr.png",
     alt: "wthrr-weather-app",
     description: `Minimalist weather app using Weather API. Written in
       vanilla Javascript.`,
     sourcecode: "https://github.com/nronzel/Wthr",
     link: "https://nronzel.github.io/Wthr/",
     icons: ["html", "css", "javascript", "webpack"],
+    includeImage: true,
   },
   {
     title: "Knights Travails",
-    src: "#",
+    src: "/knights.png",
     alt: "knights-travails",
     description: `Given a chessboard and the knight piece, this app determines
       the shortest path between any two points.`,
     sourcecode: "https://github.com/nronzel/Knights-Travails",
     link: "https://nronzel.github.io/Knights-Travails/",
     icons: ["html", "sass", "javascript", "webpack"],
+    includeImage: true,
   },
   {
     title: "Keeb Store",
-    src: "#",
+    src: "/keebstore.png",
     alt: "keeb-store",
     description: `Mock keyboard store with shopping cart.`,
     sourcecode: "https://github.com/nronzel/shopping-cart",
-    link: "keebstore.netlify.app",
+    link: "https://keebstore.netlify.app",
     icons: [
       "javascript",
       "vite",
-      "html",
+      "reactrouter",
       "chakraui",
       "react",
-      "reactrouter",
       "jest",
       "netlify",
     ],
+    includeImage: true,
   },
   {
     title: "ResumeBuildr",
-    src: "#",
+    src: "/resumebuilder.png",
     alt: "resume-builder",
     description: `Fill out the sections of the form and watch your resume
       be built in the preview.`,
     sourcecode: "https://github.com/nronzel/resume-maker",
     link: "https://nronzel.github.io/resume-maker/",
     icons: ["javascript", "vite", "html", "chakraui", "react"],
+    includeImage: true,
   },
   {
     title: "SpongeMem",
-    src: "#",
+    src: "/spongemem.png",
     alt: "memory-game",
     description: `Memory game with characters from Spongebob. What is your
       highest score?`,
     sourcecode: "https://github.com/nronzel/Memory-game",
-    link: "spongemem.netlify.app/",
+    link: "https://spongemem.netlify.app/",
     icons: ["javascript", "vite", "html", "chakraui", "react", "netlify"],
+    includeImage: true,
   },
   {
     title: "What Am I Working On?",
@@ -94,6 +100,7 @@ const projects = [
     sourcecode: "",
     link: "",
     icons: [],
+    includeImage: false,
   },
 ];
 
