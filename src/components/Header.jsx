8be73f1b00118bca03962326508ddca46ef90130
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-26 21:40:51 -0400

    add: smooth transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,23 +9,23 @@ const Header = () => {
   return (
     <nav class="flex h-16 justify-end items-center w-4/5 font-medium shrink-0">
       <ul class="flex gap-7 text-lg">
-        <li class="hover:text-emerald-200">
+        <li class="header-links hover:text-emerald-200">
           <span class="font-light">/\/ </span>
           <a href="#about" onClick={() => scrollToSection("about")}>
             about
           </a>
         </li>
-        <li class="hover:text-emerald-200">
+        <li class="header-links hover:text-emerald-200">
           <span class="font-light">/\/ </span>
           <a href="#projects">projects</a>
         </li>
-        <li class="hover:text-emerald-200">
+        <li class="header-links hover:text-emerald-200">
           <span class="font-light">/\/ </span>
           <a href="/nicholas-ronzel-resume.pdf" target="_blank">
             resume
           </a>
         </li>
-        <li class="hover:text-emerald-200">
+        <li class="header-links hover:text-emerald-200">
           <span class="font-light">/\/ </span>
           <a href="https://github.com/nronzel" target="_blank">
             github
