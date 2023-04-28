76e9a45ca71bc15e1a426a96cf9b21e457761811
nronzel <86695181+nronzel@users.noreply.github.com>
2023-04-27 21:22:35 -0400

    change: style & text

--- src/components/EmailTag.jsx
+++ src/components/EmailTag.jsx
@@ -1,6 +1,15 @@
 const EmailTag = () => {
   return (
-    <div class="email-tag simpleFade tracking-widest text-emerald-100">
+    <div
+      class="
+      email-tag
+      text-center
+      simpleFade
+      tracking-widest
+      text-emerald-100
+      "
+    >
+      <p class="font-normal">send me an email</p>
       <p>nick@nickronzel.com</p>
     </div>
   );
